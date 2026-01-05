bcd438d8797da3f56c1f7c0e8006f2d944bd19af
sumnlikeshane <shanegmain@gmail.com>
2026-01-06 00:27:50 +0530

    initial setup

---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-red-500 text-center">Welcome to Opus</h1>
+    </div>
+  );
+};
+
+export default Landing;
